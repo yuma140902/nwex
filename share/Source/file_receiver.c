9874caac52111a5f3bd779d1e55e4b913ab044d6
yuma14 <yuma140902@gmail.com>
2023-12-03 22:47:20 +0900

    refactor

--- share/Source/file_receiver.c
+++ share/Source/file_receiver.c
@@ -8,73 +8,25 @@
 /**
  * @brief コマンドライン引数をパースする
  *
- * @param[in]  argc コマンドライン引数の数
- * @param[in]  argv コマンドライン引数の内容
+ * @param[in]  argc     コマンドライン引数の数
+ * @param[in]  argv     コマンドライン引数の内容
  * @param[out] filename 受信した内容を保存するファイル名
  * @return
  * 正常にパースできたなら0、ヘルプを表示したなら1、引数が足りなかったなら2
  */
-int ParseArgs(int argc, char **argv, char **filename) {
-  if (argc == 1 || (argc == 2 && strncmp(argv[1], "-h", 2) == 0)) {
-    printf("Usage: %s filename\n", argv[0]);
-    return argc == 1 ? 2 : 1;
-  }
-  if (argc > 1)
-    *filename = argv[1];
-  return 0;
-}
+int ParseArgs(int argc, char **argv, char **filename);
 
 /**
  * @brief struct sockaddr_inを標準出力に表示する
- *
- * @param addr [TODO:parameter]
  */
-void ShowSockAddr(struct sockaddr_in *addr) {
-  char buf[80];
-  struct in_addr *addr_in_p = (struct in_addr *)&addr->sin_addr;
-  inet_ntop(AF_INET, addr_in_p, buf, sizeof(buf));
-  printf("addr: %s, port#: %d\n", buf, ntohs(addr->sin_port));
-}
+void ShowSockAddr(struct sockaddr_in *addr);
 
 /**
  * @brief 待ち受け用のソケットを準備する
  *
  * @return ソケットディスクリプタ。途中でエラーが発生した場合は負の値
  */
-int PrepareSockWait() {
-  int sock0;   /* 待ち受け用ソケットディスクリプタ */
-  int yes = 1; /* setsockopt()用 */
-  struct sockaddr_in selfAddr;
-
-  /* TCPソケットをオープンする */
-  if ((sock0 = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("socket");
-    return -1;
-  }
-
-  /* sock0のコネクションがTIME_WAIT状態でもbind()できるように設定 */
-  setsockopt(sock0, SOL_SOCKET, SO_REUSEADDR, (const char *)&yes, sizeof(yes));
-
-  /* 送信元からの要求を受け付けるIPアドレスとポートを設定する */
-  memset(&selfAddr, 0, sizeof(selfAddr));     /* ゼロクリア */
-  selfAddr.sin_family = AF_INET;              /* Internetプロトコル */
-  selfAddr.sin_port = htons(TCP_SERVER_PORT); /* 待ち受けるポート */
-  selfAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* どのIPアドレス宛でも */
-
-  /* ソケットとアドレスをbindする */
-  if (bind(sock0, (struct sockaddr *)&selfAddr, sizeof(selfAddr)) < 0) {
-    perror("bind");
-    return -1;
-  }
-
-  /* コネクションの最大同時受け入れ数を指定する */
-  if (listen(sock0, 5) != 0) {
-    perror("listen");
-    return -1;
-  }
-
-  return sock0;
-}
+int PrepareSockWait();
 
 int main(int argc, char **argv) {
   int sock0; /* 待ち受け用ソケットディスクリプタ */
@@ -139,6 +91,58 @@ int main(int argc, char **argv) {
   close(sock0);
 
   return 0;
+}
+
+int ParseArgs(int argc, char **argv, char **filename) {
+  if (argc == 1 || (argc == 2 && strncmp(argv[1], "-h", 2) == 0)) {
+    printf("Usage: %s filename\n", argv[0]);
+    return argc == 1 ? 2 : 1;
+  }
+  if (argc > 1)
+    *filename = argv[1];
+  return 0;
+}
+
+void ShowSockAddr(struct sockaddr_in *addr) {
+  char buf[80];
+  struct in_addr *addr_in_p = (struct in_addr *)&addr->sin_addr;
+  inet_ntop(AF_INET, addr_in_p, buf, sizeof(buf));
+  printf("addr: %s, port#: %d\n", buf, ntohs(addr->sin_port));
+}
+
+int PrepareSockWait() {
+  int sock0;   /* 待ち受け用ソケットディスクリプタ */
+  int yes = 1; /* setsockopt()用 */
+  struct sockaddr_in selfAddr;
+
+  /* TCPソケットをオープンする */
+  if ((sock0 = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("socket");
+    return -1;
+  }
+
+  /* sock0のコネクションがTIME_WAIT状態でもbind()できるように設定 */
+  setsockopt(sock0, SOL_SOCKET, SO_REUSEADDR, (const char *)&yes, sizeof(yes));
+
+  /* 送信元からの要求を受け付けるIPアドレスとポートを設定する */
+  memset(&selfAddr, 0, sizeof(selfAddr));     /* ゼロクリア */
+  selfAddr.sin_family = AF_INET;              /* Internetプロトコル */
+  selfAddr.sin_port = htons(TCP_SERVER_PORT); /* 待ち受けるポート */
+  selfAddr.sin_addr.s_addr = htonl(INADDR_ANY); /* どのIPアドレス宛でも */
+
+  /* ソケットとアドレスをbindする */
+  if (bind(sock0, (struct sockaddr *)&selfAddr, sizeof(selfAddr)) < 0) {
+    perror("bind");
+    return -1;
+  }
+
+  /* コネクションの最大同時受け入れ数を指定する */
+  if (listen(sock0, 5) != 0) {
+    perror("listen");
+    return -1;
+  }
+
+  return sock0;
 }
 
 /* vim: set ff=unix fenc=utf-8 : */
--- share/Source/file_sender.c
+++ share/Source/file_sender.c
@@ -10,103 +10,44 @@
 /**
  * @brief コマンドライン引数をパースする
  *
- * @param[in]       argc コマンドライン引数の数
- * @param[in]       argv コマンドライン引数の内容
+ * @param[in]       argc            コマンドライン引数の数
+ * @param[in]       argv            コマンドライン引数の内容
  * @param[in, out]  server_host_str 送信先のホスト名のデフォルト値・パース結果
- * @param[in, out]  port_num_str 送信先のポート番号のデフォルト値・パース結果
- * @param[out]      filename 送信するファイル名のパース結果
+ * @param[in, out]  port_num_str    送信先のポート番号のデフォルト値・パース結果
+ * @param[out]      filename        送信するファイル名のパース結果
  * @return
  * 正常にパースできたなら0、ヘルプメッセージを表示したなら1、引数が足りなかったなら2
  */
 int ParseArgs(int argc, char **argv, char **server_host_str,
-              char **port_num_str, char **filename) {
-  if (argc == 1 || (argc == 2 && strncmp(argv[1], "-h", 2) == 0)) {
-    printf("Usage: %s filename [dst_host] [port]\n", argv[0]);
-    return argc == 1 ? 2 : 1;
-  }
-  if (argc > 1)
-    *filename = argv[1];
-  if (argc > 2)
-    *server_host_str = argv[2];
-  if (argc > 3)
-    *port_num_str = argv[3];
-
-  return 0;
-}
+              char **port_num_str, char **filename);
 
 /**
  * @brief サーバー名とポート名を解決する
  *
  * @param[in]  host_str ホスト名
  * @param[in]  port_str ポート番号
- * @param[out] res 解決結果
+ * @param[out] res      解決結果
  * @return 正常に解決できたなら0
  */
-int ResolveAddress(char *host_str, char *port_str, struct addrinfo **res) {
-  struct addrinfo hints;
-  int err;
-
-  /* ホスト名からIPアドレスを取得する */
-  memset(&hints, 0, sizeof(hints));
-  hints.ai_family = AF_INET;
-  hints.ai_socktype = SOCK_DGRAM;
-  err = getaddrinfo(host_str, port_str, &hints, res);
-  if (err != 0) {
-    perror("getaddrinfo");
-    return 1;
-  }
-
-  return 0;
-}
+int ResolveAddress(char *host_str, char *port_str, struct addrinfo **res);
 
 /**
  * @brief struct addrinfoを標準出力に表示する
- *
- * @param[in] addrinfo
  */
-void ShowAddress(struct addrinfo *addrinfo) {
-  struct in_addr addr;
-  addr.s_addr = ((struct sockaddr_in *)(addrinfo->ai_addr))->sin_addr.s_addr;
-  printf("ip address: %s\n", inet_ntoa(addr));
-  printf("port#: %d\n",
-         ntohs(((struct sockaddr_in *)(addrinfo->ai_addr))->sin_port));
-}
+void ShowAddress(struct addrinfo *addrinfo);
 
 /**
  * @brief ソケットをオープンし、送信先に接続する
  *
- * @param addrinfo 送信先のアドレス
+ * @param[in] addrinfo 送信先のアドレス
  * @return ソケットディスクリプタ。途中でエラーが発生した場合は負の値
  */
-int PrepareSocket(struct addrinfo *addrinfo) {
-  int sock;
-
-  /* TCPソケットをオープン */
-  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    perror("socket");
-    return -1;
-  }
-
-  /* 送信先に接続（bind相当も実行） */
-  if (connect(sock, addrinfo->ai_addr, addrinfo->ai_addrlen) < 0) {
-    perror("connect");
-    return -1;
-  }
-
-  return sock;
-}
+int PrepareSocket(struct addrinfo *addrinfo);
 
 /**
  * @brief n以上の値でmの倍数である最小の値を返す
  */
-long NextMultipleOf(long n, long m) {
-  long r = n % m;
-  if (r == 0) {
-    return n;
-  } else {
-    return n + (m - r);
-  }
-}
+long NextMultipleOf(long n, long m);
 
 /**
  * @brief ファイルを分割する
@@ -120,49 +61,7 @@ long NextMultipleOf(long n, long m) {
  * @return 正常に分割できたなら0
  */
 int DivideFile(char *filename, int block_size, int num, int *ratios,
-               long *positions, long *lengths) {
-  FILE *fp;
-  long file_size;
-  int sum;
-  int i;
-  long current_pos;
-
-  fp = fopen(filename, "rb");
-  if (fp == NULL) {
-    perror("fopen");
-    return -1;
-  }
-
-  if (fseek(fp, 0, SEEK_END) != 0) {
-    perror("fseek to end");
-    return -1;
-  }
-  file_size = ftell(fp);
-  if (fseek(fp, 0, SEEK_SET) != 0) {
-    perror("fseek to head");
-    return -1;
-  }
-
-  sum = 0;
-  for (i = 0; i < num; ++i) {
-    sum += ratios[i];
-  }
-
-  current_pos = 0;
-  for (i = 0; i < num; ++i) {
-    lengths[i] = (long)((double)file_size * (double)ratios[i] / (double)sum);
-    lengths[i] = NextMultipleOf(lengths[i], block_size);
-    positions[i] = current_pos;
-    current_pos += lengths[i];
-  }
-
-  if (fclose(fp) != 0) {
-    perror("fclose");
-    return -1;
-  }
-
-  return 0;
-}
+               long *positions, long *lengths);
 
 int main(int argc, char **argv) {
   char *dst_host_str = "127.0.0.1"; /* 送信先のホスト名（文字列） */
@@ -223,6 +122,119 @@ int main(int argc, char **argv) {
 
   /* ソケットのクローズ */
   close(sock);
+
+  return 0;
+}
+
+int ParseArgs(int argc, char **argv, char **server_host_str,
+              char **port_num_str, char **filename) {
+  if (argc == 1 || (argc == 2 && strncmp(argv[1], "-h", 2) == 0)) {
+    printf("Usage: %s filename [dst_host] [port]\n", argv[0]);
+    return argc == 1 ? 2 : 1;
+  }
+  if (argc > 1)
+    *filename = argv[1];
+  if (argc > 2)
+    *server_host_str = argv[2];
+  if (argc > 3)
+    *port_num_str = argv[3];
+
+  return 0;
+}
+
+int ResolveAddress(char *host_str, char *port_str, struct addrinfo **res) {
+  struct addrinfo hints;
+  int err;
+
+  /* ホスト名からIPアドレスを取得する */
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;
+  hints.ai_socktype = SOCK_DGRAM;
+  err = getaddrinfo(host_str, port_str, &hints, res);
+  if (err != 0) {
+    perror("getaddrinfo");
+    return 1;
+  }
+
+  return 0;
+}
+
+void ShowAddress(struct addrinfo *addrinfo) {
+  struct in_addr addr;
+  addr.s_addr = ((struct sockaddr_in *)(addrinfo->ai_addr))->sin_addr.s_addr;
+  printf("ip address: %s\n", inet_ntoa(addr));
+  printf("port#: %d\n",
+         ntohs(((struct sockaddr_in *)(addrinfo->ai_addr))->sin_port));
+}
+
+int PrepareSocket(struct addrinfo *addrinfo) {
+  int sock;
+
+  /* TCPソケットをオープン */
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+    perror("socket");
+    return -1;
+  }
+
+  /* 送信先に接続（bind相当も実行） */
+  if (connect(sock, addrinfo->ai_addr, addrinfo->ai_addrlen) < 0) {
+    perror("connect");
+    return -1;
+  }
+
+  return sock;
+}
+
+long NextMultipleOf(long n, long m) {
+  long r = n % m;
+  if (r == 0) {
+    return n;
+  } else {
+    return n + (m - r);
+  }
+}
+
+int DivideFile(char *filename, int block_size, int num, int *ratios,
+               long *positions, long *lengths) {
+  FILE *fp;
+  long file_size;
+  int sum;
+  int i;
+  long current_pos;
+
+  fp = fopen(filename, "rb");
+  if (fp == NULL) {
+    perror("fopen");
+    return -1;
+  }
+
+  if (fseek(fp, 0, SEEK_END) != 0) {
+    perror("fseek to end");
+    return -1;
+  }
+  file_size = ftell(fp);
+  if (fseek(fp, 0, SEEK_SET) != 0) {
+    perror("fseek to head");
+    return -1;
+  }
+
+  sum = 0;
+  for (i = 0; i < num; ++i) {
+    sum += ratios[i];
+  }
+
+  current_pos = 0;
+  for (i = 0; i < num; ++i) {
+    lengths[i] = (long)((double)file_size * (double)ratios[i] / (double)sum);
+    lengths[i] = NextMultipleOf(lengths[i], block_size);
+    positions[i] = current_pos;
+    current_pos += lengths[i];
+  }
+
+  if (fclose(fp) != 0) {
+    perror("fclose");
+    return -1;
+  }
 
   return 0;
 }
