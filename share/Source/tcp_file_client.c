a09d2e5898e7fc63f32f1b9b19b678682da7c26c
yuma14 <yuma140902@gmail.com>
2023-12-03 21:13:05 +0900

    引数の順番を変える

--- share/Source/tcp_file_client.c
+++ share/Source/tcp_file_client.c
@@ -18,23 +18,22 @@
  * @param[in]       argv コマンドライン引数の内容
  * @param[in, out]  server_host_str サーバーのホスト名のデフォルト値・パース結果
  * @param[in, out]  port_num_str サーバーのポート番号のデフォルト値・パース結果
- * @param[in, out]  filename 保存するファイル名のデフォルト値・パース結果
- * @return 正常にパースできたなら0、ヘルプメッセージを表示したなら1
+ * @param[in, out]  filename 送信するファイル名のデフォルト値・パース結果
+ * @return
+ * 正常にパースできたなら0、ヘルプメッセージを表示したなら1、引数が足りなかったなら2
  */
 int ParseArgs(int argc, char **argv, char **server_host_str,
               char **port_num_str, char **filename) {
-  if (argc == 2 && strncmp(argv[1], "-h", 2) == 0) {
-    printf("Usage: %s [dst_host] [port] [o_filename]\n", argv[0]);
-    return 1;
+  if (argc == 1 || (argc == 2 && strncmp(argv[1], "-h", 2) == 0)) {
+    printf("Usage: %s [filename] [dst_host] [port]\n", argv[0]);
+    return argc == 1 ? 2 : 1;
   }
   if (argc > 1)
-    *server_host_str = argv[1];
+    *filename = argv[1];
   if (argc > 2)
-    *port_num_str = argv[2];
-  if (argc > 3) { /* 出力ファイルの指定 */
-    printf("set outputfile: %s", argv[3]);
-    *filename = argv[3];
-  }
+    *server_host_str = argv[2];
+  if (argc > 3)
+    *port_num_str = argv[3];
 
   return 0;
 }
@@ -81,27 +80,30 @@ void ShowAddress(struct addrinfo *addrinfo) {
 int main(int argc, char **argv) {
   char *server_host_str = "127.0.0.1"; /* サーバ名（文字列） */
   char *port_num_str = "10000";        /* ポート番号（文字列） */
-  char *filename = NULL; /* 出力ファイル名。NULLなら標準出力 */
-  int fd = 1;            /* 受信した内容の出力先 */
+  char *filename = "test.dat"; /* 出力ファイル名。NULLなら標準出力 */
+  int fd;                      /* 受信した内容の出力先 */
   char *dummy_file = "HELLO.txt"; /* ダミーのリクエストメッセージ */
 
   int sock;          /* ソケットディスクリプタ */
   char buf[BUF_LEN]; /* 受信バッファ */
   int n;             /* 読み込み／受信バイト数 */
+  int result;
 
   struct addrinfo *res; /* アドレス情報の構造体 */
 
   /* コマンドライン引数の処理 */
-  if (ParseArgs(argc, argv, &server_host_str, &port_num_str, &filename) != 0) {
+  result = ParseArgs(argc, argv, &server_host_str, &port_num_str, &filename);
+  if (result == 1) {
+    return 0;
+  } else if (result == 2) {
     return 1;
   }
 
-  if (filename != NULL) {
-    fd = open(filename, O_CREAT | O_WRONLY, 0644);
-    if (fd < 0) {
-      perror("open");
-      return 1;
-    }
+  /* ファイルを開く */
+  fd = open(filename, O_CREAT | O_WRONLY, 0644);
+  if (fd < 0) {
+    perror("open");
+    return 1;
   }
 
   if (ResolveAddress(server_host_str, port_num_str, &res) != 0) {
