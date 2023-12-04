e2338bbb361dc37629e66828345c0186dd396fab
yuma14 <yuma140902@gmail.com>
2023-12-04 14:38:12 +0900

    update launch script

--- share/Source/node1.sh
+++ share/Source/node1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./receiver.out -s node3:9999 -p 10000 -p 10001 -p 10002 -p 10003
+./receiver.out -s node3:9999 -p 10000 -p 10001 -p 10002
--- share/Source/node3.sh
+++ share/Source/node3.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./sender.out -f test100.dat -l 9999 -s 10:node1:10000 -s 50:node2:10000 -s 12:node4:10000 -s 2:node5:10000
+./sender.out -f test100.dat -l 9999 -s 1:node1:10000 -s 5:node2:10000 -s 1:node4:10000
